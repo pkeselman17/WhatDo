e9e22d72c366c455e5a56318cc24be828c3ff128
Phil Keselman <pkeselman17@gmail.com>
2023-03-31 11:06:51 -0400

    Merged heads

---
+++ PlannerAPI/migrations/versions/0ea1121e3470_merging_heads.py
@@ -0,0 +1,24 @@
+"""merging heads
+
+Revision ID: 0ea1121e3470
+Revises: 4de7e9289e4a, 95715640c775
+Create Date: 2023-03-31 11:02:53.610547
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0ea1121e3470'
+down_revision = ('4de7e9289e4a', '95715640c775')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
